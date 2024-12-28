fc6b620cee14c6cec60561f65a64e62e86e993ba
AarushiswinnerYo <156099106+AarushiswinnerYo@users.noreply.github.com>
2024-12-28 17:21:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,19 +37,19 @@ def handleClient(conn, addr):
             if x[0]=="signup":
                 signRes=functions.signUp(x[1],x[2])
                 if signRes=="Done!":
-                    conn.send("User signed up successfully")
+                    conn.send("User signed up successfully".encode(FORMAT))
                     print(f"\033[32mUser:-{x[1]} up successfully\033[0m")
                 else:
-                    conn.send("Exists")
+                    conn.send("Exists".encode(FORMAT))
             elif x[0]=="login":
                 loginRes=functions.login(x[1],x[2])
                 if loginRes=="correct":
-                    conn.send("Successfully logged in!")
+                    conn.send("Successfully logged in!".encode(FORMAT))
                     print(f"\033[32mUser:-{x[1]} logged on\033[0m")
                 elif loginRes=="User not found!":
-                    conn.send("Non-existent")
+                    conn.send("Non-existent".encode(FORMAT))
                 else:
-                    conn.sent("Incorrect password")
+                    conn.sent("Incorrect password".encode(FORMAT))
             elif x[0]=="user":
                 connectedUsers.append(x[1])
                 ipToUser[addr[0]]=x[1]
